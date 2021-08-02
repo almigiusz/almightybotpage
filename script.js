87f497efa29f4f0038997236f04b532491215ad4
Kacper <77008351+kacper299@users.noreply.github.com>
2021-08-02 18:02:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//jezyk
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+//loader
+
+var myVar;
+
+function loader() {
+	myVar = setTimeout(showPage, 600);
+}
+
+function showPage() {
+	document.getElementById('loader').style.display = 'none';
+	document.getElementById('myDiv').style.display = 'block';
+}
